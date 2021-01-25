1a60d4d81da3a32265070cdd679a4cb905c991c0
Eric Flatt <36873954+ericflatt@users.noreply.github.com>
2021-01-24 21:32:09 -0500

    preliminary logo stuff

--- src/components/attendees.js
+++ src/components/attendees.js
@@ -1,15 +1,23 @@
 import React from "react"
 
+import "../styles/attendees.scss"
+
+import aligned from "../assets/logos/aligned.png"
 
 const Attendees = () => {
-
-    return(
-        <main>
-            <h1>Attendees</h1>
-        </main>
-    )
+  return (
+    <div className="attendeeMain" style={{marginTop:"0"}}>
+      <div className="container">
+          <h1>Attendees</h1>
+        <div className="logo-grid">
+          <img src={aligned} alt="aligned" />
+          <img src={aligned} alt="aligned" />
+          <img src={aligned} alt="aligned" />
+          <img src={aligned} alt="aligned" />
+        </div>
+      </div>
+    </div>
+  )
 }
-
-
 
 export default Attendees

b134f68d07ea77f2c3bd20bdd291dc8a5ad2eef0
Eric Flatt <36873954+ericflatt@users.noreply.github.com>
2021-01-25 11:58:48 -0500

    all logos and links

--- src/components/attendees.js
+++ src/components/attendees.js
@@ -2,18 +2,213 @@ import React from "react"
 
 import "../styles/attendees.scss"
 
+// company logos
 import aligned from "../assets/logos/aligned.png"
+import antidote from "../assets/logos/antidote.png"
+import awesome from "../assets/logos/awesome.png"
+import blackmath from "../assets/logos/black-math.png"
+import blocktackle from "../assets/logos/block&tackle.png"
+import bns from "../assets/logos/bns.png"
+import buck from "../assets/logos/buck.png"
+import coatofarms from "../assets/logos/coat-of-arms.png"
+import cycymymy from "../assets/logos/cycymymy.png"
+import dash from "../assets/logos/dash.png"
+import dukeduck from "../assets/logos/duke&duck.png"
+import eido from "../assets/logos/eido.png"
+import facebook from "../assets/logos/facebook.png"
+import fern from "../assets/logos/fern.png"
+import fivestone from "../assets/logos/fivestone.png"
+import framestore from "../assets/logos/framestore.png"
+import frost from "../assets/logos/frost.png"
+import ftcc from "../assets/logos/ftcc.png"
+import gareso from "../assets/logos/gareso.png"
+import giantant from "../assets/logos/giant-ant.png"
+import goldenwolf from "../assets/logos/golden-wolf.png"
+import gretel from "../assets/logos/gretel.png"
+import gunner from "../assets/logos/gunner.png"
+import huecry from "../assets/logos/hue&cry.png"
+import imaginaryforces from "../assets/logos/if.png"
+import iv from "../assets/logos/iv.png"
+import laundry from "../assets/logos/laundry.png"
+import leoburnett from "../assets/logos/leoburnett.png"
+import leviathan from "../assets/logos/leviathan.png"
+import linetest from "../assets/logos/linetest.png"
+import lk from "../assets/logos/lk.png"
+import logan from "../assets/logos/logan.png"
+import losyork from "../assets/logos/los-york.png"
+import meptik from "../assets/logos/meptik.png"
+import meister from "../assets/logos/meister.png"
+import mvsm from "../assets/logos/awesome.png"
+import nerdo from "../assets/logos/nerdo.png"
+import notreal from "../assets/logos/not-real.png"
+import oddfellows from "../assets/logos/odd-fellows.png"
+import peprally from "../assets/logos/pep-rally.png"
+import psypop from "../assets/logos/psypop.png"
+import sarofsky from "../assets/logos/sarofsky.png"
+// import sb from "../assets/logos/sb.png"
+import scholar from "../assets/logos/scholar.png"
+import statedesign from "../assets/logos/state.png"
+import themill from "../assets/logos/the-mill.png"
+import tnb from "../assets/logos/tnb.png"
+import trollback from "../assets/logos/trollback.png"
+import wf from "../assets/logos/wf.png"
+import wonderlust from "../assets/logos/wonderlust.png"
 
 const Attendees = () => {
   return (
-    <div className="attendeeMain" style={{marginTop:"0"}}>
+    <div className="attendeeMain" style={{ marginTop: "0" }}>
       <div className="container">
-          <h1>Attendees</h1>
+        <h1>Attendees</h1>
         <div className="logo-grid">
-          <img src={aligned} alt="aligned" />
-          <img src={aligned} alt="aligned" />
-          <img src={aligned} alt="aligned" />
-          <img src={aligned} alt="aligned" />
+          <a target="_blank" href="https://www.alignedmedia.com/">
+            <img src={aligned} alt="logo" />
+          </a>
+          <a target="_blank" href="https://antidotestudio.tv/">
+            <img src={antidote} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.awesomeinc.com/">
+            <img src={awesome} alt="logo" />
+          </a>
+          <a target="_blank" href="http://blackmath.com/">
+            <img src={blackmath} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.blockandtackle.tv/">
+            <img src={blocktackle} alt="logo" />
+          </a>
+          <a target="_blank" href="https://brandnewschool.com/">
+            <img src={bns} alt="logo" />
+          </a>
+          <a target="_blank" href="https://buck.co/">
+            <img src={buck} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.coatofarmspost.com/">
+            <img src={coatofarms} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.cycymymy.com/">
+            <img src={cycymymy} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.dashstudio.net/">
+            <img src={dash} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.dukeduck.com/">
+            <img src={dukeduck} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.eido.net/">
+            <img src={eido} alt="logo" />
+          </a>
+          <a target="_blank" href="https://about.fb.com/realitylabs/">
+            <img src={facebook} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.fern.team/">
+            <img src={fern} alt="logo" />
+          </a>
+          <a target="_blank" href="https://fivestonestudios.com/">
+            <img src={fivestone} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.fortheculture.club/">
+            <img src={ftcc} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.framestore.com/?language=en">
+            <img src={framestore} alt="logo" />
+          </a>
+          <a target="_blank" href="https://frostmotion.com/">
+            <img src={frost} alt="logo" />
+          </a>
+          <a target="_blank" href="https://gareso.com/">
+            <img src={gareso} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.giantant.ca/">
+            <img src={giantant} alt="logo" />
+          </a>
+          <a target="_blank" href="https://goldenwolf.tv/">
+            <img src={goldenwolf} alt="logo" />
+          </a>
+          <a target="_blank" href="https://gretelny.com/work/">
+            <img src={gretel} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.gunner.work/">
+            <img src={gunner} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.hueandcry.tv/">
+            <img src={huecry} alt="logo" />
+          </a>
+          <a target="_blank" href="https://imaginaryforces.com/">
+            <img src={imaginaryforces} alt="logo" />
+          </a>
+          <a target="_blank" href="https://iv.studio/">
+            <img src={iv} alt="logo" />
+          </a>
+          <a target="_blank" href="https://laundrymat.tv/">
+            <img src={laundry} alt="logo" />
+          </a>
+          <a target="_blank" href="https://leoburnett.com/">
+            <img src={leoburnett} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.lvthn.com/">
+            <img src={leviathan} alt="logo" />
+          </a>
+          <a target="_blank" href="http://linetest.tv/">
+            <img src={linetest} alt="logo" />
+          </a>
+          <a target="_blank" href="https://logan.tv/">
+            <img src={logan} alt="logo" />
+          </a>
+          <a target="_blank" href="https://losyork.tv/">
+            <img src={losyork} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.loyalkaspar.com/">
+            <img src={lk} alt="logo" />
+          </a>
+          <a target="_blank" href="https://mvsm.com/">
+            <img src={mvsm} alt="logo" />
+          </a>
+          <a target="_blank" href="https://meister.tv/">
+            <img src={meister} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.meptik.com/">
+            <img src={meptik} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.themill.com/">
+            <img src={themill} alt="logo" />
+          </a>
+          <a target="_blank" href="http://www.nerdo.tv/">
+            <img src={nerdo} alt="logo" />
+          </a>
+          {/* might not be included */}
+          {/* <a target="_blank" href=""><img src={sb} alt="logo" /></a> */}
+          <a target="_blank" href="https://thenewblank.com/">
+            <img src={tnb} alt="logo" />
+          </a>
+          <a target="_blank" href="https://notreal.tv/work">
+            <img src={notreal} alt="logo" />
+          </a>
+          <a target="_blank" href="https://oddfellows.tv/">
+            <img src={oddfellows} alt="logo" />
+          </a>
+          <a target="_blank" href="http://peprally.co/">
+            <img src={peprally} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.psyop.com/">
+            <img src={psypop} alt="logo" />
+          </a>
+          <a target="_blank" href="https://sarofsky.com/">
+            <img src={sarofsky} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.helloscholar.com/">
+            <img src={scholar} alt="logo" />
+          </a>
+          <a target="_blank" href="http://statedesign.tv/">
+            <img src={statedesign} alt="logo" />
+          </a>
+          <a target="_blank" href="https://trollback.com/">
+            <img src={trollback} alt="logo" />
+          </a>
+          <a target="_blank" href="https://www.wonderlustmedia.ca/">
+            <img src={wonderlust} alt="logo" />
+          </a>
+          <a target="_blank" href="http://www.worldfamousinc.com/">
+            <img src={wf} alt="logo" />
+          </a>
         </div>
       </div>
     </div>

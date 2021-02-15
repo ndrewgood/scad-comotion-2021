470defdaf3eac444b1a763a16361acea702c9ca8
Andrew Goodridge <gandrew888@gmail.com>
2021-02-15 16:19:46 -0500

    quick updates

---
+++ src/pages/team.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import "../styles/main.scss"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="SCAD CoMotion 2021" />
+    <p>test test</p>
+  </Layout>
+)
+
+export default IndexPage

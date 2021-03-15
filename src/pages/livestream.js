81313070941195270cc3028d0bd1467963b03759
Eric Flatt <36873954+ericflatt@users.noreply.github.com>
2021-03-15 14:33:02 -0400

    livestream

--- src/pages/livestream.js
+++ src/pages/livestream.js
@@ -1,14 +1,18 @@
 import React from "react"
-
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
-
+import "../styles/livestream.scss"
+ 
 const LivestreamPage = () => (
   <Layout>
-    <SEO title="SCAD CoMotion 2021" />
-    <p>livestream page</p>
-  </Layout>
+  <main className="liveMain">
+  <div className="liveContainer iframe-container">
+  <iframe className="liveYoutube" autoPlay width="560" height="315" src="https://www.youtube.com/embed/Pn2Y2mNEkjU?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"  allowfullscreen></iframe>
+  </div>
+  <div className="liveBackgroundFade"></div>
+  <div className="liveBackground"></div>
+</main>
+</Layout>
 )
 
 export default LivestreamPage
